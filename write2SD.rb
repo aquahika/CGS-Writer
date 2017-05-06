95ee2330d9ec8d64098e17c3ecbaa64ca25e21e3
Hikaru Sugiura <aqua.hika@gmail.com>
2017-05-06 17:50:01 +0900

    Add sleep 5 sec after DD before unmount

--- write2SD.rb
+++ write2SD.rb
@@ -137,6 +137,7 @@ print "インデックスデータ書き込み中...\n"
 `diskutil unmountDisk #{diskName}`
 `sudo dd bs=512 seek=#{(0x2A04000 / bs).to_s} if=#{curDir}/raw/index of=/dev/r#{diskName}`
 
+sleep 5
 print "紋紙データ書き込み中...\n"
 `diskutil unmountDisk #{diskName}`
 `sudo dd bs=512 seek=#{(0x2A24000 / bs).to_s} if=#{curDir}/raw/chunk of=/dev/r#{diskName}`
--- writeonly.rb
+++ writeonly.rb
@@ -24,14 +24,12 @@ p curDir = File.expand_path(File.dirname(__FILE__))
 bs = 512
 
 print "インデックスデータ書き込み中...\n"
-p diskName
 `diskutil unmountDisk #{diskName}`
-p diskName
 `sudo dd bs=512 seek=#{(0x2A04000 / bs).to_s} if=#{curDir}/raw/index of=/dev/r#{diskName}`
 
-print "紋紙データ書き込み中...\n"
-p diskName
-`diskutil unmountDisk #{diskName}`
-p diskName
-`sudo dd bs=512 seek=#{(0x2A24000 / bs).to_s} if=#{curDir}/raw/chunk of=/dev/r#{diskName}`
+sleep 5
 
+print "紋紙データ書き込み中...\n"
+`diskutil unmountDisk #{diskName}`
+`sudo dd bs=512 seek=#{(0x2A24000 / bs).to_s} if=#{curDir}/raw/chunk of=/dev/r#{diskName}`
+`diskutil unmountDisk #{diskName}`
